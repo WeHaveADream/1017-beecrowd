c2a1ac7825daef29d34f107345dcfd254fc64886
WeHaveADream <71896051+WeHaveADream@users.noreply.github.com>
2022-12-05 13:46:42 -0300

    1017 beecrowd

---
+++ 1017 solved beecrowd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+	
+	int tempo, velocidade;
+	float litros;
+
+	scanf("%d %d", &tempo, &velocidade);
+
+	litros = (tempo * velocidade) / 12.0;
+
+	printf("%.3f\n", litros);
+	 
+	return 0;
+}
